fc895557f5aeeba226887b11c79b9c04deae9a42
kholoudahmedx <kholoudahmedmuhamed4@gmail.com>
2024-08-08 11:21:32 +0300

    Update login

--- login.php
+++ login.php
@@ -11,27 +11,35 @@
 			<span class="material-icons"> restaurant</span>RecipeExplorer
 		</h3>
 	</div>
+
 	<div class="container d-flex justify-content-center align-items-center">
-		<form class="d-flex flex-column" style="width: 35%" method="POST" action="index.php">
+		<form class="d-flex flex-column" method="POST" action="index.php">
+
 		  <div class="mb-3">
 		  	<label class="form-label" id="form-title"> Login to Your Account</label>
 		  </div>
+
 		  <div class="mb-3">
 		    <label for="username" class="form-label">Username</label>
 		    <input type="text" class="form-control" id="username" placeholder="Enter your username">
 		  </div>
+
 		  <div class="mb-3 ">
 		    <label for="password" class="form-label">Password</label>
 		    <input type="password" class="form-control" id="password" placeholder="Enter your password">
 		  </div>
+
 		  <div class="mb-3">
-		    <label class="form-label">Don't have an account? <a href="#">Register here</a></label>
+		    <label class="form-label">Don't have an account? <a href="Register.php">Register here</a></label>
 		  </div">
+
 	  	  <div class="d-grid gap-2 ">
-	  	  	<button class="btn submit" type="button sumbit" name="submit">Login</button>
+	  	  	<button class="btn submit" type="button submit" name="submit">Login</button>
 	  	  </div>
+
 		</form>
 	</div>
+
 	<?php include('templates/footer.php'); ?>
 </body>
 </html>
