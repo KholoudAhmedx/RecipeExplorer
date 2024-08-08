f065e0c6baf797d63579b71d83f1e34fdb7a3660
kholoudahmedx <kholoudahmedmuhamed4@gmail.com>
2024-08-08 10:23:26 +0300

    Add header file for modularity

---
+++ templates/header.php
@@ -0,0 +1,18 @@
+<head>
+	<title>RecipeExplorer</title>
+
+	<!--To make the code responsive-->
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">  
+
+
+    <!--Icons-->
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <!-- Bootstrap CDN jsdelivr-->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+	
+	<!--Css file-->
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
