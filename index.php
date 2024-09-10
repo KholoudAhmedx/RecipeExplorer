7f94e48824acfb58c83a972a24583f914672ded3
kholoudahmedx <kholoudahmedmuhamed4@gmail.com>
2024-09-11 00:15:47 +0300

    Update navbar

--- index.php
+++ index.php
@@ -31,50 +31,47 @@ if($count > 0)
 
  </head>
  <body>
-
-
-  <nav class="navbar navbar-light fixed-top">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">
+  <nav class="navbar fixed-top navbar-expand-md navbar-light">
+    <div class="container-xxl">
+      <a class="navbar-brand" href="index.php">
         <span class="material-icons"> restaurant</span>RecipeExplorer
       </a>
 
-      <a class="navbar-item nav-link" href="#">
-        Egyption
-      </a>
-      <a class="navbar-item nav-link" href="#">
-        Lebanon
-      </a>
-      <a class="navbar-item nav-link" href="#">
-        Moroccon 
-      </a>
-      
-      <!-- To add new recipes you have to be logged in-->
-      <button class="btn" type="btn" onclick="window.location.href='<?php echo isset($_SESSION['username']) ? 'addrecipe.php' : 'login.php'; ?>'">Add Recipe </button>
-
-      <!--Drop menue for login & register buttons--> 
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+      <!-- Toggle button for mobile div -->
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-nav" aria-controls="main-nav" aria-expanded="false" aria-label="Toggle navigation">
+        <!-- Button toggler -->
+        <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <!-- navbar links -->
+      <div class="collapse navbar-collapse justify-content-end align-center" id="main-nav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="<?php echo isset($_SESSION['username']) ? 'profile.php' : 'login.php'; ?>">My Profile</a> 
+            </li> 
+            <li class="nav-item">
+              <a class="nav-link" href="Register.php">Register </a>
+            </li>
+            
+            <!--Show login/logout buttons based on the status of the user-->
+            <?php if(isset($_SESSION['username'])): ?>
+            <li class="nav-item">
+              <a class="nav-link" href="logout.php">Logout </a>
+            </li>
+            <?php else: ?>
+            <li class="nav-item">
+              <a class="nav-link" href="login.php">Login </a>
+            </li>
+            <?php endif; ?>
 
-          <!--Show login/logout buttons based on the status of the user-->
-          <?php if(isset($_SESSION['username'])): ?>
-            <li class="nav-item"><a class="nav-link" href="logout.php">Logout </a></li>
-          <?php else: ?>
-            <li class="nav-item"><a class="nav-link" href="login.php">Login </a></li>
-          <?php endif; ?>
-
-          <!--Register-->
-          <li class="nav-item"><a class="nav-link" href="Register.php">Register </a></li>
-          <li class="nav-item">
-            <button class="btn nav-link" type="btn" onclick="window.location.href='<?php echo isset($_SESSION['username']) ? 'profile.php' : 'login.php'; ?>'">My Profile</button>
-          </li>
-        </ul>
+            <li class="nav-item">
+              <a class="nav-link" href="<?php echo isset($_SESSION['username']) ? 'addrecipe.php' : 'login.php'; ?>"> Add Recipe</a>
+            </li>
+          </ul>
+        
       </div>
     </div>
-  </nav> 
+  </nav>
+  
 
   <!-- Display images from the database: -->
   <main>
@@ -83,7 +80,7 @@ if($count > 0)
       <div class="image">
         <img src="<?php $filename=$recipe['imagefile']; $imageURL="uploads/".$filename; echo $imageURL;?>">
       </div>
-      <div class="caption ">
+      <div class="caption">
         <!--author -->
         <p class="title"><strong><?php echo htmlspecialchars($recipe['title']); ?></strong></p>
         <p class="description"><?php echo htmlspecialchars($recipe['description']); ?></p>
